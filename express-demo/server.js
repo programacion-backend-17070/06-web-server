17ddd038aea02773e71724bfd6e12a168f64da4e
lalomx <lalo0892@gmail.com>
2022-01-28 09:10:53 -0600

    after

--- express-demo/server.js
+++ express-demo/server.js
@@ -6,17 +6,29 @@ const Contenedor = require("./contenedor.js")
 
 const clase = new Contenedor()
 
-app.get("/", (req, res) => {
-  res.send('<h1 style="color: green">Hola</h1>')
+app.get("/productos", async (req, res) => {
+  // let data;
+  // clase.getAll().then(d => {
+  //   data = d
+  // })
+  // .catch(err => console.log(err))
+
+  try {
+    const data = await clase.getAll()
+  } catch (err) {
+    console.log(err)
+  }
+  
+  res.send(JSON.stringify(data))
 })
 
-app.get("/api/cursos", (req, res) => {
-  res.send(JSON.stringify([1, 2, 3, 4]))
-})
+// app.get("/api/cursos", (req, res) => {
+//   res.send(JSON.stringify([1, 2, 3, 4]))
+// })
 
-app.get('/fyi', (req, res) => {
-  res.send(moment().format("HH:mm:ss DD/MM/YYYY"))
-})
+// app.get('/fyi', (req, res) => {
+//   res.send(moment().format("HH:mm:ss DD/MM/YYYY"))
+// })
 
 
 const PORT = process.env.PORT || 8080
